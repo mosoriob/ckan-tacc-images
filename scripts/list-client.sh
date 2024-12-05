6a9552a910665523d4a0e9f483ed099184ad7169
Maximiliano Osorio <maxiosorio@gmail.com>
2024-12-05 15:19:40 +0000

    fix: create client parameters callback

--- scripts/list-client.sh
+++ scripts/list-client.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # Check if correct number of arguments provided
-if [ "$#" -ne 4 ]; then
-    echo "Usage: $0 username password client-name dev-flag"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 username password client-name"
     exit 1
 fi
 
 USERNAME=$1
 PASSWORD=$2
-CLIENT_NAME=$3
-CALLBACK_URL=$4
 
 # Get JWT token
 echo "Getting JWT token..."
@@ -22,12 +20,10 @@ if [ -z "$JWT" ] || [ "$JWT" = "null" ]; then
     exit 1
 fi
 
-# Create client
-echo "Creating client... ${CLIENT_NAME}"
+# Get client information
+echo "Getting client information for ${CLIENT_NAME}..."
 
 RESPONSE=$(curl -s -H "X-Tapis-Token: $JWT" \
-    -H "Content-type: application/json" \
-    -d "{\"client_id\": \"$CLIENT_NAME\", \"callback_url\": \"$CALLBACK_URL\"}" \
     https://portals.tapis.io/v3/oauth2/clients)
 
 echo "Response:"
